32f50f74ddd3111b96e35847125940ce74c574db
Kevin Gandhi <kevin.gandhi@somaiya.edu>
2024-10-18 00:27:51 +0530

    Added TailwindCSS and some minor fixes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors:{
+      grey:'#e6e9eb',
+      blue:'#214656;',
+    }
+  },
+  plugins: [],
+}
+
